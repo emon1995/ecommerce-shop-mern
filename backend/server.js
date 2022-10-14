de8e22665a68dc8897ae7a3226870282f2838f29
emon1995 <emonhossain995@gmail.com>
2022-10-14 20:13:09 +0600

    env and es6 module add

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,24 @@
-const express = require("express");
-const products = require("./data/products");
+import dotenv from "dotenv";
+import express from "express";
+import products from "./data/products.js";
 
 const app = express();
+const PORT = process.env.PORT || 5000;
+dotenv.config();
 
 app.get("/", (req, res) => {
-    res.send("API is running...")
-})
+  res.send("API is running...");
+});
 
 app.get("/api/products", (req, res) => {
-    res.json(products);
-})
+  res.json(products);
+});
 
 app.get("/api/products/:id", (req, res) => {
-    const product = products.find(p => p._id === req.params.id);
-    res.json(product);
-})
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
 
-app.listen(5000, () => {
-    console.log('Server running on port 5000');
-})
+app.listen(PORT, () => {
+  console.log(`Server running in ${process.env.NODE_ENV} on port --> ${PORT}`);
+});
