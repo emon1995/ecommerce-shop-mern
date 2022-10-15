9562f74dc07d5e36d81eb29eb49b3901958e88c5
emon1995 <emonhossain995@gmail.com>
2022-10-15 20:20:54 +0600

    Redux single product add

--- backend/routes/productRoutes.js
+++ backend/routes/productRoutes.js
@@ -11,8 +11,8 @@ router.get(
   "/",
   asyncHandler(async (req, res) => {
     const products = await Product.find({});
-    // res.status(401)
-    // throw new Error('Not Authorized');
+    // res.status(401);
+    // throw new Error("Not Authorized");
     res.json(products);
   })
 );
