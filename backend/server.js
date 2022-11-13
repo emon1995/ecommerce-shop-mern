fd2de9586ef151f9ca396e7b7b5b4358907dfc2e
emon1995 <emonhossain995@gmail.com>
2022-11-13 20:58:41 +0600

    yes

--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,9 @@
-import path from 'path';
+import path from "path";
 import dotenv from "dotenv";
 import express from "express";
 // import cors from 'cors';
 import colors from "colors";
-import morgan from 'morgan';
+import morgan from "morgan";
 import connectDB from "./config/db.js";
 import productRoutes from "./routes/productRoutes.js";
 import userRoutes from "./routes/userRoutes.js";
@@ -19,16 +19,12 @@ dotenv.config();
 connectDB();
 
 // morgan middleware
-if(process.env.NODE_ENV === 'development'){
-  app.use(morgan('dev'))
+if (process.env.NODE_ENV === "development") {
+  app.use(morgan("dev"));
 }
 
 // app.use(cors());
 app.use(express.json());
-
-app.get("/", (req, res) => {
-  res.send("API is running...");
-});
 
 // Auth route
 app.use("/api/users", userRoutes);
@@ -44,7 +40,19 @@ app.get("/api/config/paypal", (req, res) =>
 );
 
 const __dirname = path.resolve();
-app.use('/uploads', express.static(path.join(__dirname, '/uploads')));
+app.use("/uploads", express.static(path.join(__dirname, "/uploads")));
+
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "/frontend/build")));
+
+  app.get("*", (req, res) =>
+    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+  );
+} else {
+  app.get("/", (req, res) => {
+    res.send("API is running...");
+  });
+}
 
 // error middleware
 app.use(notFound);
